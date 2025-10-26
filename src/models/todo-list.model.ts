d3fe5f6bac234afbe0e0b9294c7a7761426ae7be
Jason Warner <xocomil@gmail.com>
2025-10-26 12:15:09 -0600

    feat(todo-list): Wrap TodoList model in an object

--- src/models/todo-list.model.ts
+++ src/models/todo-list.model.ts
@@ -1,17 +1,27 @@
 import { z } from 'zod';
 import { todoFactory, todoParser } from './todo.model';
 
-export const todoListParser = z.array(todoParser);
+export const todoListParser = z.object({
+  todos: z.array(todoParser),
+});
 
 export type TodoList = z.infer<typeof todoListParser>;
 
 export function todoListFactory(): TodoList {
-  return [
-    todoFactory(),
-    todoFactory({
-      id: 2,
-      name: 'Learn @angular/aria',
-      description: "Because it's cool",
-    }),
-  ];
+  return {
+    todos: [
+      todoFactory(),
+      todoFactory({
+        id: 2,
+        name: 'Learn @angular/aria',
+        description: "Because it's cool",
+      }),
+      todoFactory({
+        id: 3,
+        name: 'Explain factories vs const objects',
+        description: 'Jason likes factories for lots of reasons',
+        done: true,
+      }),
+    ],
+  };
 }
