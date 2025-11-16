04d3b80c9c5b44d7738896ab8b126d0bacd659b3
Jason Warner <xocomil@gmail.com>
2025-11-16 10:55:43 -0700

    Adds defer block to todo form
    Adds a defer block around the todo form to improve initial load time.
    
    Renames `field` input to `fieldTree` on the `app-input` component.
    This allows the input component to work with nested form structures.

--- src/components/todo.ts
+++ src/components/todo.ts
@@ -34,50 +34,52 @@ import { ProjectedInput, ProjectedInputStyles } from './projected-input';
       }
     </div>
     <hr />
-    <form
-      class="grid grid-cols-[30px_90px_minmax(120px,_1fr)_60px_90px_120px] gap-x-2 items-baseline flex-wrap max-w-[75%]"
-    >
-      <input
-        class="checkbox checkbox-success row-span-2"
-        [field]="form.done"
-        type="checkbox"
-      />
-      <input
-        class="input w-20"
-        [class.input-error]="form.id().invalid()"
-        [field]="form.id"
-        placeholder="id"
-        type="number"
-      />
-      <!--      <app-input-->
-      <!--        [field]="form.name"-->
-      <!--        label="Task Name"-->
-      <!--        placeholder="Name your task"-->
-      <!--      />-->
-      <app-projected-input [errors]="form.name().errors()" label="Task Name">
+    @defer {
+      <form
+        class="grid grid-cols-[30px_90px_minmax(120px,_1fr)_60px_90px_120px] gap-x-2 items-baseline flex-wrap max-w-[75%]"
+      >
         <input
-          [field]="form.name"
-          [class.input-error]="form.name().invalid()"
-          appDefaultStyle
-          placeholder="Name your task"
-          type="text"
+          class="checkbox checkbox-success row-span-2"
+          [field]="form.done"
+          type="checkbox"
         />
-      </app-projected-input>
+        <input
+          class="input w-20"
+          [class.input-error]="form.id().invalid()"
+          [field]="form.id"
+          placeholder="id"
+          type="number"
+        />
+        <!--      <app-input-->
+        <!--        [field]="form.name"-->
+        <!--        label="Task Name"-->
+        <!--        placeholder="Name your task"-->
+        <!--      />-->
+        <app-projected-input [errors]="form.name().errors()" label="Task Name">
+          <input
+            [field]="form.name"
+            [class.input-error]="form.name().invalid()"
+            appDefaultStyle
+            placeholder="Name your task"
+            type="text"
+          />
+        </app-projected-input>
 
-      <app-random-number
-        [field]="form.randomNumber"
-        [minValue]="150"
-        [maxValue]="250"
-      />
-      <input class="input w-20" [field]="form.taskImportance" type="number" />
-      <app-star-input [field]="form.taskImportance" />
-      <app-input
-        class="col-span-5"
-        [field]="form.description"
-        label="Description"
-        placeholder="Describe your task"
-      />
-    </form>`,
+        <app-random-number
+          [field]="form.randomNumber"
+          [minValue]="150"
+          [maxValue]="250"
+        />
+        <input class="input w-20" [field]="form.taskImportance" type="number" />
+        <app-star-input [field]="form.taskImportance" />
+        <app-input
+          class="col-span-5"
+          [fieldTree]="form.description"
+          label="Description"
+          placeholder="Describe your task"
+        />
+      </form>
+    }`,
   styles: ``,
   host: {
     class: 'block ml-2 p-1',
