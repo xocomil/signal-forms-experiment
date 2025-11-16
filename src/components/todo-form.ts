b57aa45c7d123fbe387f1ad9726e857ea5803f88
Jason Warner <xocomil@gmail.com>
2025-11-16 11:57:30 -0700

    Splits todo component into view and form
    Refactors the todo component to separate the display of todos from the todo creation form.
    
    Introduces a new component specifically for handling the todo form, improving component organization and separation of concerns.

--- src/components/todo-form.ts
+++ src/components/todo-form.ts
@@ -1,10 +1,76 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Field, form } from '@angular/forms/signals';
+import { Input } from '../controls/input';
+import { RandomNumber } from '../controls/random-number';
+import { StarInput } from '../controls/star-input';
+import { todoFactory, TodoModel, todoSchema } from '../models/todo.model';
+import { ProjectedInput, ProjectedInputStyles } from './projected-input';
 
 @Component({
   selector: 'app-todo-form',
-  imports: [],
-  template: `<p>todo-form works!</p>`,
+  imports: [
+    Input,
+    ProjectedInput,
+    ProjectedInputStyles,
+    RandomNumber,
+    StarInput,
+    Field,
+    JsonPipe,
+  ],
+  template: `
+    <form
+      class="grid grid-cols-[30px_90px_minmax(120px,_1fr)_60px_90px_120px] gap-x-2 items-baseline flex-wrap max-w-[75%]"
+    >
+      <input
+        class="checkbox checkbox-success row-span-2"
+        [field]="form.done"
+        type="checkbox"
+      />
+      <input
+        class="input w-20"
+        [class.input-error]="form.id().invalid()"
+        [field]="form.id"
+        placeholder="id"
+        type="number"
+      />
+      <!--      <app-input-->
+      <!--        [field]="form.name"-->
+      <!--        label="Task Name"-->
+      <!--        placeholder="Name your task"-->
+      <!--      />-->
+      <app-projected-input [errors]="form.name().errors()" label="Task Name">
+        <input
+          [field]="form.name"
+          [class.input-error]="form.name().invalid()"
+          appDefaultStyle
+          placeholder="Name your task"
+          type="text"
+        />
+      </app-projected-input>
+
+      <app-random-number
+        [field]="form.randomNumber"
+        [minValue]="150"
+        [maxValue]="250"
+      />
+      <input class="input w-20" [field]="form.taskImportance" type="number" />
+      <app-star-input [field]="form.taskImportance" />
+      <app-input
+        class="col-span-5"
+        [fieldTree]="form.description"
+        label="Description"
+        placeholder="Describe your task"
+      />
+    </form>
+    <hr />
+    <pre>{{ form().errorSummary() | json }}</pre>
+  `,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TodoForm {}
+export class TodoForm {
+  protected model = signal<TodoModel>(todoFactory());
+
+  protected form = form(this.model, todoSchema);
+}
--- src/components/todo.ts
+++ src/components/todo.ts
@@ -1,32 +1,13 @@
 import { JsonPipe } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  signal,
-} from '@angular/core';
-import { Field, form } from '@angular/forms/signals';
-import { Input } from '../controls/input';
-import { RandomNumber } from '../controls/random-number';
-import { StarInput } from '../controls/star-input';
-import { todoFactory, TodoModel, todoSchema } from '../models/todo.model';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { TodoModel } from '../models/todo.model';
 import { TodoListStore } from '../stores/todo-list.store';
-import { ProjectedInput, ProjectedInputStyles } from './projected-input';
+import { TodoForm } from './todo-form';
 
 @Component({
   selector: 'app-todo',
-  imports: [
-    Field,
-    Input,
-    JsonPipe,
-    ProjectedInput,
-    ProjectedInputStyles,
-    RandomNumber,
-    StarInput,
-  ],
-  template: `<pre>{{ form().errorSummary() | json }}</pre>
-    <hr />
-    <div>
+  imports: [JsonPipe, TodoForm],
+  template: ` <div>
       @for (todo of store.todos(); track todo.id) {
         <pre class="hover:bg-primary/10" (click)="handleTodoClick(todo)">{{
           todo | json
@@ -35,50 +16,7 @@ import { ProjectedInput, ProjectedInputStyles } from './projected-input';
     </div>
     <hr />
     @defer {
-      <form
-        class="grid grid-cols-[30px_90px_minmax(120px,_1fr)_60px_90px_120px] gap-x-2 items-baseline flex-wrap max-w-[75%]"
-      >
-        <input
-          class="checkbox checkbox-success row-span-2"
-          [field]="form.done"
-          type="checkbox"
-        />
-        <input
-          class="input w-20"
-          [class.input-error]="form.id().invalid()"
-          [field]="form.id"
-          placeholder="id"
-          type="number"
-        />
-        <!--      <app-input-->
-        <!--        [field]="form.name"-->
-        <!--        label="Task Name"-->
-        <!--        placeholder="Name your task"-->
-        <!--      />-->
-        <app-projected-input [errors]="form.name().errors()" label="Task Name">
-          <input
-            [field]="form.name"
-            [class.input-error]="form.name().invalid()"
-            appDefaultStyle
-            placeholder="Name your task"
-            type="text"
-          />
-        </app-projected-input>
-
-        <app-random-number
-          [field]="form.randomNumber"
-          [minValue]="150"
-          [maxValue]="250"
-        />
-        <input class="input w-20" [field]="form.taskImportance" type="number" />
-        <app-star-input [field]="form.taskImportance" />
-        <app-input
-          class="col-span-5"
-          [fieldTree]="form.description"
-          label="Description"
-          placeholder="Describe your task"
-        />
-      </form>
+      <app-todo-form />
     }`,
   styles: ``,
   host: {
@@ -89,10 +27,6 @@ import { ProjectedInput, ProjectedInputStyles } from './projected-input';
 })
 export class Todo {
   protected readonly store = inject(TodoListStore);
-
-  protected model = signal<TodoModel>(todoFactory());
-
-  protected form = form(this.model, todoSchema);
 
   protected handleTodoClick(todo: TodoModel) {
     alert(JSON.stringify(todo));
